a892cd35772ea8959494d57bf196841b1c8eca1a
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2024-02-09 01:04:18 +0600

    Add files via upload

---
+++ BInary to Text Converter/code.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int binaryToDecimal(char bin[])
+{
+    int i, j;
+    int len = strlen(bin);
+    int dec = 0;
+    int temp;
+
+    for (i = 0, j = len - 1; i < len; i++, j--) {
+        if (bin[i] == '1') {
+            temp = pow(2, j);
+            dec = dec + temp;
+        }
+        else if (bin[i] == '0') {
+            dec = dec;
+        }
+        else {
+            printf("Invalid value as argument!\n");
+            return 1;
+        }
+    }
+    return dec;
+}
+
+
+
+int main()
+{
+    char bin[9] = "1234";
+    int dec = binaryToDecimal(bin);
+    char ch = dec;
+    printf("%c\n", ch);
+    return 0;
+}
