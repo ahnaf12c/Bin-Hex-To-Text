edc3bdd45a78803a517056b73737f48660a75edc
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2024-02-09 18:46:01 +0600

    Add files via upload

--- BInary to Text Converter/code.c
+++ BInary to Text Converter/code.c
@@ -9,6 +9,11 @@ int binaryToDecimal(char bin[])
     int dec = 0;
     int temp;
 
+    if (len > 8) {
+        printf("Invalid value as argument!\n");
+        return 1;
+    }
+
     for (i = 0, j = len - 1; i < len; i++, j--) {
         if (bin[i] == '1') {
             temp = pow(2, j);
@@ -17,7 +22,7 @@ int binaryToDecimal(char bin[])
         else if (bin[i] == '0') {
             dec = dec;
         }
-        else {
+        else if (bin[i] != '1' || bin[i] != '0') {
             printf("Invalid value as argument!\n");
             return 1;
         }
@@ -30,9 +35,18 @@ int binaryToDecimal(char bin[])
 int main()
 {
     char bin[9];
-    gets(bin);
-    int dec = binaryToDecimal(bin);
-    char ch = dec;
-    printf("%c\n", ch);
+
+    printf("WELCOME TO MY BINARY TO TEXT CONVERTER!\n V ALPHA 1.0\nSTILL WORK IN PROGRESS\n\n");
+    while(1) {
+        printf("Please enter an eight bit binary number (0 to exit): ");
+        gets(bin);
+        if (strlen(bin) == 1 && bin[0] == '0') {
+            printf("BYE! HAVE AGREAT TIME!\n");
+            break;
+        }
+        int dec = binaryToDecimal(bin);
+        char ch = dec;
+        printf("%c\n", ch);
+    }
     return 0;
 }
