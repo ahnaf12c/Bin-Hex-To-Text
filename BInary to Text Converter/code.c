628267e7ee91b2a9f33a40234730aa09792dfcf9
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2024-02-14 01:29:27 +0600

    Add files via upload

--- BInary to Text Converter/code.c
+++ BInary to Text Converter/code.c
@@ -2,6 +2,15 @@
 #include<string.h>
 #include<math.h>
 
+#define MAX_LEN 10000  // Maximum length of the binary input string
+
+/**
+ * @brief Converts a binary string to a decimal integer.
+ *
+ * @param bin The binary string to be converted.
+ * @return The decimal equivalent of the binary string.
+ *         Returns -1 if the input is invalid.
+ **/
 int binaryToDecimal(char bin[])
 {
     int i, j;
@@ -9,11 +18,13 @@ int binaryToDecimal(char bin[])
     int dec = 0;
     int temp;
 
+    // Validate input length
     if (len > 8) {
         printf("Invalid value as argument!\n");
-        return 1;
+        return -1;
     }
 
+    // Convert binary to decimal
     for (i = 0, j = len - 1; i < len; i++, j--) {
         if (bin[i] == '1') {
             temp = pow(2, j);
@@ -22,31 +33,76 @@ int binaryToDecimal(char bin[])
         else if (bin[i] == '0') {
             dec = dec;
         }
-        else if (bin[i] != '1' || bin[i] != '0') {
+        else if (bin[i] != '1' && bin[i] != '0') {
             printf("Invalid value as argument!\n");
-            return 1;
+            return -1;
         }
     }
     return dec;
 }
 
-
-
-int main()
+/**
+ * @brief Converts binary input to text.
+ *
+ * Reads a string of eight-bit binary numbers separated by spaces from the user,
+ * converts each group of eight bits to its corresponding ASCII character,
+ * and prints the resulting text.
+ */
+void binToText()
 {
-    char bin[9];
+    char bin[MAX_LEN], binTemp[9], ch;
+    int dec;
+    int i, j;
 
-    printf("WELCOME TO MY BINARY TO TEXT CONVERTER!\n V ALPHA 1.0\nSTILL WORK IN PROGRESS\n\n");
+    printf("WELCOME TO MY BINARY TO TEXT CONVERTER!\nV ALPHA 1.5\nSTILL WORK IN PROGRESS\n\n");
     while(1) {
-        printf("Please enter an eight bit binary number (0 to exit): ");
+        // User Prompt
+        printf("Please enter a string of eight bit binary numbers separated by spaces ('q' to exit): ");
         gets(bin);
-        if (strlen(bin) == 1 && bin[0] == '0') {
-            printf("BYE! HAVE AGREAT TIME!\n");
+
+        // Check for exit condition
+        if (strlen(bin) == 1 && bin[0] == 'q') {
+            printf("BYE! HAVE A GREAT TIME!\n");
             break;
         }
-        int dec = binaryToDecimal(bin);
-        char ch = dec;
-        printf("%c\n", ch);
+
+        //Processing the binary input
+        for (i = 0, j = 0; i < strlen(bin); i++) {
+            if (bin[i] != ' ') {
+                binTemp[j] = bin[i];
+                j++;
+            }
+            else if (j > 0) {
+                binTemp[j] == '\0';
+                dec = binaryToDecimal(binTemp);
+                if (dec == -1) {
+                    break;
+                }
+                else if (dec > 0){
+                    ch = dec;
+                    printf("%c", ch);
+                }
+                j = 0;
+            }
+        }
+
+        //Print remaining characters
+        if (j > 0) {
+            binTemp[j] = '\0';
+            dec = binaryToDecimal(binTemp);
+            if (dec > 0) {
+                ch = dec;
+                printf("%c", ch);
+            }
+        }
+
+        printf("\n");
     }
+}
+
+//Main Function
+int main()
+{
+    binToText();
     return 0;
 }
